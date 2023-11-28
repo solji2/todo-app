e9970e02054852cc61ffef5d7be06fde16c1d572
solji2 <thfwl0094@naver.com>
2023-11-29 00:06:42 +0900

    20231129 TodoInsert css설정

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { MdAdd } from "react-icons/md";
+import "./TodoInsert.scss";
+
 const TodoInsert = () => {
   return (
-    <form>
+    <form className="TodoInsert">
       <input placeholder="할 일을 입력하세요." />
       <button type="submit">
         <MdAdd />
