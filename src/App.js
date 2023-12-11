904f9e9d910336754423621df2ca79578a334f89
solji2 <thfwl0094@naver.com>
2023-12-12 00:16:55 +0900

    20231212 todoInsert

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoList from "./components/TodoList";
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 function App() {
   const [todos, setTodos] = useState([
     //todos 상태 정의하여 TodoList 컴포넌트의 props로 전달하기
@@ -22,10 +22,30 @@ function App() {
       checked: false,
     },
   ]);
+
+  //교윳값으로 사용될 id
+  //ref를 사용하여 변수 담기
+  //useState가 아닌 useRef를 사용한 이유 -> id값은 렌더링되는 정보가 아니기때문
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+
+      setTodos(todos.concat(todo));
+      nextId.current += 1; //1씩 더하기
+    },
+    [todos]
+  );
+
   return (
     <div>
       <TodoTemplate>
-        <TodoInsert />
+        <TodoInsert onInsert={onInsert} />
         <TodoList todos={todos} />
       </TodoTemplate>
     </div>
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -2,7 +2,7 @@ import React, { useCallback, useState } from "react";
 import { MdAdd } from "react-icons/md";
 import "./TodoInsert.scss";
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState("");
 
   const onChange = useCallback((e) => {
@@ -10,8 +10,18 @@ const TodoInsert = () => {
     console.log(e.target.value);
     setValue(e.target.value);
   }, []);
+
+  const onSubmint = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue(""); //value 값 초기화
+      e.preventDefault(); //submit이벤트는 브라우저에서 새로고침을 발생시키므로 이를 방지하기 위한 함수 호출
+    },
+    [onInsert, value]
+  );
+
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmint}>
       <input
         placeholder="할 일을 입력하세요."
         value={value}
