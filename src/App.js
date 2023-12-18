ab4fc520c92daf6a34a776fb3f29b3eb83e3512c
solji2 <thfwl0094@naver.com>
2023-12-19 01:08:06 +0900

    20231219 컴포넌트 성는 최적화-useReducer 사용

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoList from "./components/TodoList";
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useReducer, useRef, useState } from "react";
 
 function createBulkTodos() {
   const array = []; // 배열 선언
-  for (let i = 1; i <= 2500; i++) {
+  for (let i = 1; i <= 5; i++) {
     array.push({
       id: i,
       text: `할 일${i}`,
@@ -16,45 +16,50 @@ function createBulkTodos() {
   return array;
 }
 
+//useReducer 사용하기
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case "INSERT": //추가
+      return todos.concat(action.todo);
+    case "REMOVE": //제거
+      return todos.filter((todo) => todo.id !== action.id);
+    case "TOGGLe": //토글
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
+      );
+    default:
+      break;
+  }
+}
+
 const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+  //const [todos, setTodos] = useState(createBulkTodos);
+
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   //교윳값으로 사용될 id
   //ref를 사용하여 변수 담기
   //useState가 아닌 useRef를 사용한 이유 -> id값은 렌더링되는 정보가 아니기때문
-  const nextId = useRef(2501);
+  const nextId = useRef(6);
 
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
 
-      setTodos(todos.concat(todo));
-      nextId.current += 1; //1씩 더하기
-    },
-    [todos]
-  );
+    dispatch({ type: "INSERT", todo });
+    nextId.current += 1; //1씩 더하기
+  }, []);
 
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id)); //선택한 row의 id값이 일치하지 않는것만 state에 저장
-    },
-    [todos]
-  );
+  const onRemove = useCallback((id) => {
+    dispatch({ type: "REMOVE", id });
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo
-        )
-      );
-    },
-    [todos]
-  );
+  const onToggle = useCallback((id) => {
+    dispatch({ type: "TOGGLe", id });
+  }, []);
 
   return (
     <div>
